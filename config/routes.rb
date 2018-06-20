b6629426aa75fed44cda1528a50fe52629aa2f31
Michael Boroski <mjboroski@gmail.com>
2018-06-20 22:55:01 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch "/posts/:id", to: "posts#update"
-
 end
